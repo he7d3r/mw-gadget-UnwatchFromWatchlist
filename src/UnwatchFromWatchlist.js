8d674f92719c15410e579675f3ec8e43cc51cae3
Helder <he7d3r@gmail.com>
2014-09-20 11:22:28 -0300

    Remove tracking link (only useful on wiki)

--- src/UnwatchFromWatchlist.js
+++ src/UnwatchFromWatchlist.js
@@ -5,7 +5,6 @@
  *
  * @source: [[mw:Snippets/Unwatch_from_watchlist]]
  * @rev: 1
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/UnwatchFromWatchlist.js]] ([[File:User:Helder.wiki/Tools/UnwatchFromWatchlist.js]])
  */
 /*jshint browser: true, camelcase: true, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: true, nonew: true, quotmark: true, undef: true, unused: true, strict: true, trailing: true, maxlen: 120, evil: true, laxbreak: true, onevar: true */
 /*global jQuery, mediaWiki */
