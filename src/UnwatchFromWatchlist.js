5300c8b7299a16eaee9bef3fcdf62f04f4627db1
Helder <he7d3r@gmail.com>
2014-09-27 20:49:03 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/UnwatchFromWatchlist.js
+++ src/UnwatchFromWatchlist.js
@@ -7,31 +7,31 @@
  * @rev: 1
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function addUnwatchlink(){
-	// Get the links
-	var $wlHistLinks = $( '#content' ).find( 'ul.special > li > a[href$="action=history"]');
-	$.each( $wlHistLinks, function() {
-		var $el = $( this ), // Cache the result instead of calling $() again
-			$unwatch = $el.clone()
-				.text( 'unwatch' )
-				.css('color', 'gray')
-				/*jshint unused:false */
-				.attr( 'href', function( i, val ) {
-					return val.replace( 'action=history', 'action=unwatch' );
-				} );
-				/*jshint unused:true */
-		$el.after( $unwatch ).after( ' | ' );
-	});
-}
+	function addUnwatchlink() {
+		// Get the links
+		var $wlHistLinks = $( '#content' ).find( 'ul.special > li > a[href$="action=history"]');
+		$.each( $wlHistLinks, function () {
+			var $el = $( this ), // Cache the result instead of calling $() again
+				$unwatch = $el.clone()
+					.text( 'unwatch' )
+					.css('color', 'gray')
+					/*jshint unused:false */
+					.attr( 'href', function ( i, val ) {
+						return val.replace( 'action=history', 'action=unwatch' );
+					} );
+					/*jshint unused:true */
+			$el.after( $unwatch ).after( ' | ' );
+		});
+	}
 
-if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Watchlist'
-	&& window.location.href.indexOf( '/edit' ) === -1
-	&& window.location.href.indexOf( '/raw' ) === -1
-) {
-	// Only on Watchlist and not in the /edit or /raw mod
-	$( addUnwatchlink );
-}
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Watchlist'
+		&& window.location.href.indexOf( '/edit' ) === -1
+		&& window.location.href.indexOf( '/raw' ) === -1
+	) {
+		// Only on Watchlist and not in the /edit or /raw mod
+		$( addUnwatchlink );
+	}
 
 }( mediaWiki, jQuery ) );
