725548357571fd361e1e16b7ead307de2cd303e6
Helder <he7d3r@gmail.com>
2014-11-22 10:50:35 -0200

    Use "mw.hook( 'wikipage.content' ).add" instead of $
    This should allow it to be compatible with scripts which reload the watchlist periodically

--- src/UnwatchFromWatchlist.js
+++ src/UnwatchFromWatchlist.js
@@ -1,17 +1,18 @@
 /**
- * Unwatch from watchlist ([[bugzilla:424]])
- * Unwatchlink per item on watchlist (adds " | unwatch" for each entry)
- * Rewritten by Krinkle (2011-01-31)
+ * Unwatch from watchlist
  *
+ * Add an "unwatch" link near each entry on the watchlist view ([[bugzilla:424]]).
+ *
+ * @author Krinkle, 2011-2014
  * @source: [[mw:Snippets/Unwatch_from_watchlist]]
- * @rev: 1
+ * @revision 2014-11-22
  */
 ( function ( mw, $ ) {
 	'use strict';
 
-	function addUnwatchlink() {
+	function addUnwatchlink( $content ) {
 		// Get the links
-		var $wlHistLinks = $( '#content' ).find( 'ul.special > li > a[href$="action=history"]');
+		var $wlHistLinks = $content.find( 'ul.special > li > a[href$="action=history"]');
 		$.each( $wlHistLinks, function () {
 			var $el = $( this ), // Cache the result instead of calling $() again
 				$unwatch = $el.clone()
@@ -31,7 +32,7 @@
 		&& window.location.href.indexOf( '/raw' ) === -1
 	) {
 		// Only on Watchlist and not in the /edit or /raw mod
-		$( addUnwatchlink );
+		mw.hook( 'wikipage.content' ).add( addUnwatchlink );
 	}
 
 }( mediaWiki, jQuery ) );
